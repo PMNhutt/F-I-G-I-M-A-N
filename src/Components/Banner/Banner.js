594fbbb55e773dd55d90881fb729ef768de8a4c9
Phan Minh Nhựt <phanminhnhut.114@gmail.com>
2022-04-16 15:10:23 +0700

    update banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,7 +6,15 @@ function Banner() {
 
     return (
         <div className="banner">
-            <div className="container"></div>
+            <div className="container">
+                <div className="banner-left-cover"></div>
+                <div className="content">
+                    <h1>Mô Hình Độc Đáo Chất Lượng Cao</h1>
+                    <h3>Mang đến cho bạn cảm giác mua sắm hoàn hảo</h3>
+
+                    <a><button className="banner-btn">Khám Phá Ngay</button></a>
+                </div>
+            </div>
         </div>
     )
 }
